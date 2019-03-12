82acc73258c4bf5eb044cf26487f59f8497ffff1
carlaight <charlesmix23@gmail.com>
2019-03-12 15:44:38 +0100

    Stripe is installed, need to be connect with registrations later

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,8 @@ Rails.application.routes.draw do
   root 'home#index'
   get 'dashboard', to: "dashboard#index"
   
+
+  #Stripe
+  resources :charges
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
