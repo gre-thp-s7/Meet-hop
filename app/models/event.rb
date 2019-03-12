93dbaa97c790fa62353d641f62d348ce4fff538e
JungleWilly <guillaumec.dev@gmail.com>
2019-03-12 19:12:06 +0100

    [done] relations table done

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,2 +1,11 @@
 class Event < ApplicationRecord
+  belongs_to :locality
+
+  has_many :category_events
+  has_many :categories, through: :category_events
+
+  has_many :registrations, dependent: :destroy
+  has_many :users, through: :registrations
+
+  belongs_to :promoter, class_name:'User'
 end
