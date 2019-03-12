30169a810afb2031ba6a3e78748a460bfd7f58fe
dendiflet <bretin.iannis@gmail.com>
2019-03-12 16:49:44 +0100

    [in progress] User model & event model first shot

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,67 @@
 class User < ApplicationRecord
+
+	before_save :avatar_picture
+  before_save { self.email = email.downcase }
+
+
+	validates :first_name, presence: true
+	validates :last_name, presence: { message: "must be given connard " }
+	validates :phone, presence: true, allow_blank: true
+
+
+############## try by yaya for validation ##############@
+  validates :nick_name,
+    uniqueness: {
+      object = self
+      data = { model: "User", attribute: "nick_name", value: :nick_name }
+      message: ->(object, data) do
+        "Hey #{object.first_name} !, #{data[:value]} est déja pris, essaye encore ! "
+      end
+    },
+
+#########@ this is documentation wher i find this ################    
+##### https://guides.rubyonrails.org/active_record_validations.html#common-validation-options ####
+
+
+######  email validation #########
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,
+    presence: true,
+    length: { maximum: 255 },
+    format: { with: VALID_EMAIL_REGEX },
+    uniqueness: { case_sensitive: false }
+######## end email validation #############
+
+
+###### this is profile picture with active storage ######
+  has_one_attached :avatar
+#####################################
+
+
+
+#=================== MAILER =================
+# Send an email after a user is created
+# after_create :welcome_email_send
+
+# def welcome_email_send
+
+#   # Tell the UserMailer to send a welcome email after save
+#   UserMailer.welcome_email(self).deliver_now
+
+# end
+#=============================================
+
+
+private
+
+######### put a profile picture if there isn't #########
+	def avatar_picture
+			if avatar_url == nil
+				avatar_url = aws_s3_no_profile_picture_url
+			end
+	end
+#################################################
+
+
+
 end
