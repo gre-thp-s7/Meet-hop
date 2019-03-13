e7e46fc8f4c360f07ef77cfa2fe82b5ffbf86eca
dendiflet <bretin.iannis@gmail.com>
2019-03-13 11:45:52 +0100

    added the god user in the seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,6 +8,8 @@
 
 require 'faker'
 
+
+################### these lignes clear the DB ##############@
 User.destroy_all
 ActiveRecord::Base.connection.reset_pk_sequence!('users')
 Event.destroy_all
@@ -20,8 +22,21 @@ CategoryEvent.destroy_all
 ActiveRecord::Base.connection.reset_pk_sequence!('category_events')
 Locality.destroy_all
 ActiveRecord::Base.connection.reset_pk_sequence!('localities')
+##################################################################
 
 
+###### this is super user (admin and devellopper) ##########
+  User.create!(
+  first_name: "first_name",
+  last_name: "last_name",
+  nick_name: "my_name_is_god",
+  avatar_url: "",
+  phone: "01 47 20 00 01",
+  is_god: true,
+  email: "thp.sess7.gre@gmail.com",
+  password: "azeaze"
+  )
+#################################################  
 
 10.times do |u|
   u = User.create!(
@@ -44,7 +59,7 @@ end
   )
 end
 
-10.times do |e|
+3.times do |e|
   e = Event.create!(
   name: Faker::Nation.nationality,
   description: Faker::Restaurant.description,
@@ -68,7 +83,7 @@ end
   )
 end
 
-15.times do |c|
+10.times do |c|
   c = Category.create!(
     name: Faker::Restaurant.name
   )
