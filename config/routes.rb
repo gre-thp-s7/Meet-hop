0bbc6d8df89c7bc6823fc544febe6f8a3dcc4a06
JungleWilly <guillaumec.dev@gmail.com>
2019-03-13 15:58:47 +0100

    [modify] charges_controller and config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  get 'registrations/index'
+  get 'registrations/new'
+  get 'registrations/show'
+  get 'registrations/create'
+  get 'registrations/edit'
+  get 'registrations/destroy'
   get 'users/new'
   get 'users/index'
   get 'users/create'
@@ -11,6 +17,10 @@ Rails.application.routes.draw do
   devise_for :users, controllers: {
     sessions: 'users/sessions'
   }
+
+  resources :users
+  resources :events
+  
 
   #Stripe
   resources :charges
