98cb8c43197873e080e02061ee9c0000ca607a13
Maxime Audfray <maxime.audfray@gmail.com>
2019-03-13 14:13:01 +0100

    [start] layout on every page

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  get 'users/new'
+  get 'users/index'
+  get 'users/create'
+  get 'users/destroy'
+  get 'users/update'
+  get 'users/show'
   root 'home#index'
   get 'dashboard', to: "dashboard#index"
-  
+
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
 
   #Stripe
   resources :charges
