825f13d3ef4c4e6de3e7c5267d3fd1e0bc28f154
dendiflet <bretin.iannis@gmail.com>
2019-03-13 11:02:38 +0100

    users have created_event method now

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,8 +8,11 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  has_many :registrations
+  has_many :registrations, dependent: :destroy
   has_many :events, through: :registrations
+
+  has_many :created_events, class_name: "Event", foreign_key: 'promoter_id', dependent: :destroy 
+
 
 
   validates :first_name, presence: true
