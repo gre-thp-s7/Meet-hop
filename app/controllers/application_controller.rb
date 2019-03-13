0b9d82fd29376a0c93e0e1171aaa13e598001d29
dendiflet <bretin.iannis@gmail.com>
2019-03-13 15:38:23 +0100

    [done] eventually, alert bootstrap are effective/ready to use

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,7 @@
 class ApplicationController < ActionController::Base
   layout "landing"
 
+# ajout des quatres alertes principale en bootstrap (3autres manquantes)
+  add_flash_types :danger, :info, :warning, :success
+
 end
