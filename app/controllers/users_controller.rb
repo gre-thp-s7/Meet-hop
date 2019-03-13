b3c334cdcf343d1925781f1e6bcae3436c537b56
dendiflet <bretin.iannis@gmail.com>
2019-03-13 17:41:19 +0100

    [done] user profile can be shown and be edited

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,9 +11,38 @@ class UsersController < ApplicationController
   def destroy
   end
 
+  def edit
+    @user = current_user
+    @profile = User.find_by(user_id: @user.id)    
+  end
+
   def update
+    @user = current_user
+    @profile = User.find_by(user_id: @user.id)
+    puts "**"*100
+    puts @current_page
+    puts @params
+    puts "**"*100
+    post_params = params[:profile]
+
+    if @profile.update(first_name: post_params[:first_name], last_name: post_params[:last_name], phone: post_params[:phone])
+      flash[:notice] = "Vous avez bien mis à jour votre profil"
+
+      redirect_to request.referrer
+    else
+      if @profile.errors.any?
+        @profile.errors.full_messages.each do |message|
+          flash[:error] =  "#{message}"
+        end
+      end
+      redirect_to request.referrer
+    end
+
   end
 
   def show
+    @user = current_user
+    @profile = User.find_by(user_id: @user.id)      
   end
+
 end
