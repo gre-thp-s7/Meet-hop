cd36b43cb842fa7b50697ba71c78c8d34eb12a97
dendiflet <bretin.iannis@gmail.com>
2019-03-14 11:52:22 +0100

    [in progress] new method whosit created in application controller, called in event crontroller & border tested in devise registration around the form

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,45 +6,48 @@ class ApplicationController < ActionController::Base
   add_flash_types :danger, :info, :warning, :success
 
 
-############## add by yaya have to modify them to use them 
+############## add by yaya : have to TTTEEESSSSTTT them to use them 
 private
 
   def authenticate_user
     unless current_user
-      flash[:danger] = "Connecte toi stp !"
+      flash[:danger] = "Connecte toi stp con***** !"
       redirect_to new_session_path
     end
   end
 
-  # def is_admin?
-  #   puts "@"*60
-  #   puts "admin?"
-  #   post_params = params.permit(:id)
-  #   event = Event.find_by(id: params[:id])
-  #   @user.id == event.admin_id
-  # end 
+  def is_promotor_of_the_event
+    puts "@"*60
+    puts "admin?"
+    puts "this method looks if your the creator of the event"
+    post_params = params.permit(:id)
+    event = Event.find_by(id: params[:id])
+    current_user.id == event.promoter_id
+  end 
 
-  # def already_subscribed?
-  #   post_params = params.permit(:id)
-  #   @event = Event.find_by(id: params[:id])
-  #   puts "@"*60
-  #   puts "déja inscrit?"
-  #   if @event.attendances.find_by(user_id: current_user.id) != nil
-  #     return true
-  #   else
-  #     return false
-  #   end
-  # end
+  def already_subscribed_to_the_event
+    post_params = params.permit(:id)
+    @event = Event.find_by(id: params[:id])
+    puts "@"*60
+    puts "déja inscrit?"
+    puts "this method looks if your already participant of the event"    
+    if @event.registration.find_by(user_id: current_user.id) != nil
+      return true
+    else
+      return false
+    end
+  end
 
-  # def can_subscribe_for_event?
-  #   puts "@"*60
-  #   puts "pas admin et pas inscrit ?" 
-  #   if is_admin? || already_subscribed?
-  #     return false
-  #   else
-  #     return true
-  #   end
-  # end
+  def can_subscribe_for_event
+    puts "@"*60
+    puts "pas admin et pas inscrit ?" 
+    puts "this method looks if you can subscribe to the event"    
+    if is_admin || already_subscribed
+      return false
+    else
+      return true
+    end
+  end
 ###################################################
 
 
--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,71 +2,83 @@ class EventsController < ApplicationController
   
   # for later with piture and active storage
   require 'open-uri'
-  
-  before_action :authenticate_user, only: [:new, :create]
-  before_action :authenticate_admin, only: [:edit, :update, :destroy]
 
-def index
-  @events = Event.all
-  @nb_events = Event.all.length
-  @last_event1 = Event.find_by(id: @nb_events)
-  @last_event2 = Event.find_by(id: @nb_events-1)
-  @last_event3 = Event.find_by(id: @nb_events-2)
-end
+###### protection against path in search browser bar ###### 
+  before_action :authenticate_user!, only: [:new, :create]
+  before_action :is_promotor, only: [:edit, :update, :destroy]
+############################################################
 
-
-def show
-  binding.pry 
-  #post_params = params.permit.....
-  @event = Event.find(params[:id])
-end
-
-def new
-  @new_event = Event.new
-end
-
-def create
-  binding.pry
-  #post_params = params.require(:new_event).permit(all)
-  @new_event = Event.new
-
-  if @new_event.save!
-    flash[:success] = "évènement créé !"
-    redirect_to(root_path)
+############ method to create variable for "if" in front ####
+### it give boolean for each ###########
+  def whosit
+    @promotor = is_promotor_of_the_event
+    @already_participant = already_subscribed_to_the_event
+    @subscribtion_possible = can_subscribe_for_event    
   end
-end
+#############################################
 
-# GET /events/:id/edit with (edit.html.erb)
-def edit
-  binding.pry 
-  #post_params = params.permit(:id)
-  @event_to_edit = Event.find_by(id: params[:id])
-end
-
-
-def update
-  posted_params = params.permit(:event)
-  params.permit(:id)
-
-  @event_to_update = Event.find_by(id: params[:id])
-  ############################
-  if @event_to_update.update(title: params[:event][:title],description: params[:event][:description],start_date: params[:event][:start_date],duration: params[:event][:duration],price: params[:event][:price],location: params[:event][:location])
-    flash[:success] = "Ton évenement a bien été modifié !"
-    redirect_to event_path(@id)
-  else
-    render :edit
+  def index
+    @events = Event.all
+    @nb_events = @events.length
+    @last_event1 = Event.find_by(id: @nb_events)
+    @last_event2 = Event.find_by(id: @nb_events-1)
+    @last_event3 = Event.find_by(id: @nb_events-2)
   end
-end
-#################################
 
+  def show
+    whosit
+    binding.pry 
+    #post_params = params.permit.....
+    @event = Event.find(params[:id])
 
-# DELETE events/:id
-def destroy
-  @event.destroy
-  respond_to do |format|
-    format.html { redirect_to events_path, notice: 'Event was successfully destroyed.' }
   end
-end
+
+  def new
+    @new_event = Event.new
+  end
+
+  def create
+    binding.pry
+    #post_params = params.require(:new_event).permit(all)
+    @new_event = Event.new
+
+    if @new_event.save!
+      flash[:success] = "évènement créé !"
+      redirect_to(root_path)
+    end
+  end
+
+  # GET /events/:id/edit with (edit.html.erb)
+  def edit
+    binding.pry 
+    #post_params = params.permit(:id)
+    @event_to_edit = Event.find_by(id: params[:id])
+  end
+
+
+  def update
+    posted_params = params.permit(:event)
+    params.permit(:id)
+
+    @event_to_update = Event.find_by(id: params[:id])
+    ############################
+    if @event_to_update.update(title: params[:event][:title],description: params[:event][:description],start_date: params[:event][:start_date],duration: params[:event][:duration],price: params[:event][:price],location: params[:event][:location])
+      flash[:success] = "Ton évenement a bien été modifié !"
+      redirect_to event_path(@id)
+    else
+      render :edit
+    end
+  end
+  #################################
+
+
+  # DELETE events/:id
+  def destroy
+    @event.destroy
+    respond_to do |format|
+      format.html { redirect_to events_path, notice: 'Event was successfully destroyed.' }
+    end
+  end
 
 =begin
 def registration
@@ -107,12 +119,14 @@ end
 
 
 private
-# Use callbacks to share common setup or constraints between actions.
-def set_event
-  @event = Event.find(params[:id])
-end
 
-# def event_params
-#   params.require(:event).permit()
-# end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_event
+    @event = Event.find(params[:id])
+  end
+
+  # def event_params
+  #   params.require(:event).permit()
+  # end
+
 end
