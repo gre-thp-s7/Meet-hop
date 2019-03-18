19fb01d635bbee125f4ea9e271192ebeab9383f3
dendiflet <bretin.iannis@gmail.com>
2019-03-18 16:52:11 +0100

    [done] registration are working, they arent nested so html secu breach, categories are ready to be registered in create#charges in the future table

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -1,26 +1,23 @@
 class ChargesController < ApplicationController
-=begin 
-The controller does two things:
--------------------------------
-  Shows a credit card form (using Checkout).
-  Creates the actual charges by calling our API.
-=end
 
 def new
-    binding.pry
     params.permit(:event, :danseur, :category)
     @event = Event.find(params[:event])
-    @categories = params[:category]
+    @categories = []
+    binding.pry
+    @categories = params[:categories]
     @amount = "3700"
 end
 
 def create
-  binding.pry
-  params.permit(:amount, :event)
+  params.permit(:amount, :event, :categories)
   @amount = params[:amount]
   @event_id = params[:event]
+  @categories = params[:categories].split
   @babar = current_user.id
-
+    puts "@"*60
+    puts "voici les catégories auquel s'est inscrit le danseur"
+    puts " l'array d'id des danses <%= @categories %>"
 
   customer = Stripe::Customer.create(
     :email => params[:stripeEmail],
@@ -34,23 +31,17 @@ def create
     :currency    => 'eur'
   )
 
-  @registration = Registration.new
-  @registration.user_id = @babar
-  @registration.event_id = @event_id
-  @registration.save
-  puts "@"*60
-  puts "registration crée"
-  
-  rescue Stripe::CardError => e
-  flash[:error] = e.message
-  binding.pry
-  redirect_to new_registration_path
-  end
+    @registration = Registration.new
+    @registration.user_id = @babar
+    @registration.event_id = @event_id
+    @registration.save
+    puts "@"*60
+    puts "registration crée"
 
-
-
-
-
-
+    rescue Stripe::CardError => e
+    flash[:error] = e.message
+    binding.pry
+    redirect_to new_registration_path
+    end
 
 end
