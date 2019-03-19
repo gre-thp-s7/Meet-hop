87a2228f22a5dc6d499601f3c4a027ab43af5f18
Maxime Audfray <maxime.audfray@gmail.com>
2019-03-19 12:37:38 +0100

    [progress] done some basic front

--- db/schema.rb
+++ db/schema.rb
@@ -101,7 +101,6 @@ ActiveRecord::Schema.define(version: 2019_03_18_214253) do
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
-    t.string "facebook_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
