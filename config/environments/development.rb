4bb377f0637f14a551d96a8263c185adb0f8acf0
Maxime Audfray <maxime.audfray@gmail.com>
2019-03-21 13:26:13 +0100

    [git] merge into stagingtest corrected

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -15,7 +15,7 @@ config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # In the development environment your application's code is reloaded on
   # every request. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
-  config.cache_classes = true
+  config.cache_classes = false
 
   # Do not eager load code on boot.
   config.eager_load = false
@@ -59,13 +59,6 @@ config.action_mailer.default_url_options = { host: 'localhost', port: 3000 }
   # This option may cause significant delays in view rendering with a large
   # number of complex assets.
   config.assets.debug = true
-  config.serve_static_assets = true
-  config.assets.compile = true
-  config.assets.digest = true
-
-
-
-
 
   # Suppress logger output for asset requests.
   config.assets.quiet = true
