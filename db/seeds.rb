01ad95060755d96507c3586a1a2859989bdd83a1
dendiflet <bretin.iannis@gmail.com>
2019-03-23 04:47:37 +0100

    [upgrade for nothing] categories colors are good now (and the each is refacto), krump is unique now

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,14 +3,14 @@
 ################### these lignes clear the DB ##############@
 User.destroy_all
 ActiveRecord::Base.connection.reset_pk_sequence!('users')
-Event.destroy_all
-ActiveRecord::Base.connection.reset_pk_sequence!('events')
-Registration.destroy_all
-ActiveRecord::Base.connection.reset_pk_sequence!('registrations')
+#Event.destroy_all
+# ActiveRecord::Base.connection.reset_pk_sequence!('events')
+# Registration.destroy_all
+# ActiveRecord::Base.connection.reset_pk_sequence!('registrations')
 Category.destroy_all
 ActiveRecord::Base.connection.reset_pk_sequence!('categories')
-CategoryEvent.destroy_all
-ActiveRecord::Base.connection.reset_pk_sequence!('category_events')
+# CategoryEvent.destroy_all
+# ActiveRecord::Base.connection.reset_pk_sequence!('category_events')
 ##################################################################
 I18n.reload!
 
@@ -35,19 +35,19 @@ Category.create!(name: "voguing")
 Category.create!(name: "toprock")
 
 # on peut passer `confirmed_at: Time.now` pour confirmer tes users dans le seed si on a confirmation par email dans devise
-10.times do |u|
-  u = User.create!(
-  first_name: Faker::Name.first_name,
-  last_name: Faker::Name.last_name,
-  nick_name: Faker::ElectricalComponents.unique.active,
-  avatar_url: "",
-  phone: "01 47 20 00 01",
-  is_god: false,
-  email: "teamgre38#{u}@yopmail.com",
-  password: "123456"
-  )
-  puts "User #{u.first_name}"
-end
+# 10.times do |u|
+#   u = User.create!(
+#   first_name: Faker::Name.first_name,
+#   last_name: Faker::Name.last_name,
+#   nick_name: Faker::ElectricalComponents.unique.active,
+#   avatar_url: "",
+#   phone: "01 47 20 00 01",
+#   is_god: false,
+#   email: "teamgre38#{u}@yopmail.com",
+#   password: "123456"
+#   )
+#   puts "User #{u.first_name}"
+# end
 # 10.times do |u|
 #   u = User.create!(
 #   first_name: Faker::Name.first_name,
